11717d03c91cfbd731316887b69ff2dbebc30159
Code-Crafting-Hub <yashkaransingh0827@gmail.com>
2026-02-10 08:32:00 +0530

    new

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Logo from "../components/Logo";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <>
       <Logo />
       <Navbar />
+      <Footer/>
     </>
   );
 }

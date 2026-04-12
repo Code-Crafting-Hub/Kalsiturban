81d8fec16eef26f85ef05a14e0355858d3e73d26
Code-Crafting-Hub <yashkaransingh0827@gmail.com>
2026-04-12 19:15:48 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -66,6 +66,7 @@ const login = async (req, res) => {
     const cookieOptions = {
       expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
       httpOnly: true,
+      path: "/",
       secure: true,      // MUST be true for HTTPS (Vercel)
       sameSite: "none",
     };

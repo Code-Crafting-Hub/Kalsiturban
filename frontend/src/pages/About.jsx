312d260d30f5af0c384b6ef4ff3cde73d0bb62f4
Code-Crafting-Hub <yashkaransingh0827@gmail.com>
2026-04-27 10:13:21 +0530

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 // Import your team images
 import img1 from '../assets/person1.jpeg'; // Content Specialist
-import img2 from '../assets/person2.jpeg'; // UI & Programmer
+import img2 from '../assets/person2.png'; // UI & Programmer
 import img3 from '../assets/person3.png'; // Project File Manager
 
 export default function About() {
